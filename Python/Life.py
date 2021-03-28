2beb76415baccdd5e60cc65a877c0f9bcb5ba1e6
LeviRicardo <leviricardogsilva@gmail.com>
2021-03-28 00:07:26 -0300

    Nothing relevant

--- Python/Life.py
+++ Python/Life.py
@@ -1,17 +1,30 @@
 from random import choice as rd
+from time import sleep as slp
 
-def montar_tabuleiro(a=15,l=50):
-    chances = 2
-    possibilidades = [" " if i!= round(100/chances) else "#" for i in range(1, round(100/chances)+1)]
-    tabuleiro = [ [] for i in range(a)]
-    for linhas in range(len(tabuleiro)):
-        tabuleiro[linhas] = "".join([rd(possibilidades) for i in range(l)])
-    return(tabuleiro)   
+def mount_board(h=15,w=50): # Creates a board using the parameters as size
+    chances = 3 #Chances of life existing in percentage
+    possibilities = [" " if i!= round(100/chances) else "#" for i in range(1, round(100/chances)+1)]
+    board = [ [] for i in range(h)]
+    for linhas in range(len(board)):
+        board[linhas] = [rd(possibilities) for i in range(w)]
+    return(board)   
 
-def proximo_estagio(board=montar_tabuleiro()):
+def next_board(board):
+    next_board = board
     
 
 
 
+    return next_board
 
-[print(i) for i in proximo_estagio()]
+first_board = mount_board()
+present_board = first_board
+att_time = 0.1
+
+
+print(first_board)
+for i in range(5):
+#    print("-------------------------------------------------------------")
+    present_board = next_board(present_board)
+    [print("".join(i)) for i in present_board]
+    slp(att_time)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GameOfLife
 
 Creating conways game of lifeboth in **Python** and **Javascript**
+
+# Work Still In Progress

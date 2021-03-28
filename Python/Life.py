61cde88e44eaf4d5341f2332f811fc5e5d84fa77
LeviRicardo <leviricardogsilva@gmail.com>
2021-03-28 18:58:46 -0300

    Boards bugs are weird

--- Python/Life.py
+++ Python/Life.py
@@ -8,46 +8,48 @@ def mount_board(h=15,w=50, chances=3): # Creates a board using the parameters as
     board = [ [] for i in range(h)]
     for rows in range(len(board)):
         board[rows] = [rd(possibilities) for i in range(w)]
-    return(board)   
+    return board 
 
-def next_board(board):
-    next_board = list(board)
-    for rows in range(len(board)):
-        for columns in range(len(board[rows])):
-            total = checking(board,rows,columns)
-            if next_board[rows][columns] == "#":
-                if total < 2 or total > 3:
-                    next_board[rows][columns] = " "
-                else:
-                    next_board[rows][columns] = "#"
-            else:
-                if total == 3:
-                    next_board[rows][columns] = "#"
+def next_board(board2):
+    next_board = []
+    next_board.extend(board2)
+    for rows in range(len(board2)):
+        for columns in range(len(board2[rows])):
+            total = int(checking(board2,rows,columns))
+            if board2[rows][columns] == "#" and total not in [2,3]:
+                print("Morre")
+                #next_board[rows][columns] = " "
+            elif board2[rows][columns] == "#" and total in [2,3]:
+                print("Vive")
+                #next_board[rows][columns] = "#"
+            elif board2[rows][columns] == " " and total == 3:
+                print("Revive")
+                #next_board[rows][columns] = "#"
     return next_board
 
-def checking(board, row, column):
-    board = board[:]
+def checking(board3, row, column):
+    board4 = board3[:]
     indexes = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
     counter = 0
     for i in indexes:
         try:
             if row+i[0] < 0 or column+i[1]<0:
                 continue
-            if board[row+i[0]][column+i[1]] == "#":
+            if board4[row+i[0]][column+i[1]] == "#":
                 counter += 1
         except IndexError:
             pass
     return counter
 
-first_board = mount_board(h=5,w=5,chances=20)
-present_board = first_board
+#first_board = mount_board(h=5,w=5,chances=20)
+#present_board = first_board
 att_time = 0.1
-rounds = 3
+rounds = 1
 present_board = [["#","#","#"," "," "],[" "," "," "," "," "],[" "," "," "," "," "],[" "," "," "," "," "],[" "," "," "," "," "]]
 
-[print("".join(i)) for i in present_board]
+#[print("".join(i)) for i in present_board]
 for i in range(rounds):
-    print("-------------------------------------------------------------")
+ #   print("-------------------------------------------------------------")
     present_board = next_board(present_board)
-    [print("".join(i)) for i in present_board]
-    slp(att_time)
+  #  [print("".join(i)) for i in present_board]
+   # slp(att_time)
